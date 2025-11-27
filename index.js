96b440fd3b1452018ac3b039ba849669f386a574
chisu-kawahara <chisu.kawahara@gmail.com>
2025-11-27 12:26:51 -0800

    fetch background img data from api and add in style

---
+++ index.js
@@ -0,0 +1,6 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature", {method: "GET"})
+	.then (res => res.json())
+	.then (data => 
+		document.body.style.backgroundImage = `url(${data.urls.full})`
+		);
+	
